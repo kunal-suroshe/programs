a9efdba109eec1bdaea65aa55025ac4c7d20c22d
kunal_s <128156983+kunal-suroshe@users.noreply.github.com>
2023-05-22 14:47:22 +0530

    Add files via upload

---
+++ day4/calculator.js
@@ -0,0 +1,8 @@
+function sum() {
+    let input1 = document.querySelector("#id1").value;
+    let input2 = document.querySelector("#id2").value;
+  
+    let result = parseInt(input1) + parseInt(input2);
+  
+    alert("Input1 " + input1 + " Input2 " + input2 + " Result =" + result);
+  }
